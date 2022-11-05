ddce2ea29ce56d76ea623dd3f20071f579419c28
Shivam Saini <shivamsn97@gmail.com>
2022-11-05 12:43:31 +0530

    Fixed based on feedback.

--- __tests__/test_todo_app.js
+++ __tests__/test_todo_app.js
@@ -8,17 +8,25 @@ const oneDay = 60 * 60 * 24 * 1000;
 const yesterday = new Date(today.getTime() - 1 * oneDay);
 const tomorrow = new Date(today.getTime() + 1 * oneDay);
 
+/*
+Feedback from Pranshu Aggarwal(Teaching Assistant - Web Development)
+- You are not adding a new todo in this test, you are just verifying the count of todos
+
+Fixed: by expecting length to be zero before running all the tests and adding new todos in the respective test.
+*/
+
 describe("TodoList Test Suite by shivamsn97@gmail.com", () => {
   beforeAll(() => {
+    expect(all.length).toBe(0);
+  });
+
+  test("Should add new tests correctly.", () => {
     add({ title: "Pay rent", dueDate: yesterday.toLocaleDateString("en-CA") });
     add({
       title: "Service vehicle",
       dueDate: today.toLocaleDateString("en-CA"),
     });
     add({ title: "File taxes", dueDate: tomorrow.toLocaleDateString("en-CA") });
-  });
-
-  test("Should add new tests correctly.", () => {
     expect(all.length).toBe(3);
   });
 
